d826b3f53a16e8982b7fa7eb31a14223a5cd6aca
tdzsx789 <405094616@qq.com>
2024-05-19 03:41:28 +0800

    update: 金融大屏完成首頁

---
+++ jinrongdaping/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Time from './components/timer';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Time />
+    </div>
+  );
+}
+
+export default App;
---
+++ jinrongdaping/src/components/timer/index.js
@@ -0,0 +1,34 @@
+import { useEffect, useState, memo, useRef } from "react";
+import calendar from "js-calendar-converter";
+import styles from "./index.module.scss";
+
+const App = memo(function App() {
+  const [time, setTime] = useState(new Date());
+  const [date, setDate] = useState({week: '', date: ''});
+
+  useEffect(() => {
+    const _newDate = new Date();
+    const _date = calendar.solar2lunar(_newDate);
+    setDate({
+        week: _date.ncWeek,
+        date: _date.date
+    })
+
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval); // Cleanup the interval on component unmount
+  }, []);
+
+  console.log("_date", date);
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.time}>{time.toLocaleTimeString()}</div>
+      <div className={styles.date}>{date.date}</div>
+      <div className={styles.week}>{date.week}</div>
+    </div>
+  );
+});
+
+export default App;
